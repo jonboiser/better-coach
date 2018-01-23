19417825aacfb4c04c7b8c28866f51ed0d3db480
Jonathan Boiser <jboiser@gmail.com>
2018-01-23 13:54:55 -0800

    First commit + basic exercise template

---
+++ src/components/Quiz.vue
@@ -0,0 +1,70 @@
+<template>
+
+  <div>
+    <h1>The Quiz</h1>
+    <div class="exercise">
+      <p>{{ exerciseQuestion }}</p>
+      <div class="responses">
+        <template v-for="i in range">
+          <input
+            v-model="selectedResponse"
+            type="radio"
+            name="response"
+            :value="i"
+            :id="`response${i}`"
+            :key="i"
+            :disabled="submitted"
+          />
+          <label
+            :for="`response${i}`"
+            :key="`label${i}`"
+          >Answer {{ i }}
+          </label>
+          <br :key="`br${i}`">
+        </template>
+      </div>
+    </div>
+    <div class="buttons">
+      <button
+        v-if="!submitted"
+        type="submit"
+        @click="submitted=true"
+      >
+        Submit
+      </button>
+      <button
+        v-if="submitted"
+        @click="submitted=false"
+      >
+        Next
+      </button>
+    </div>
+  </div>
+
+</template>
+
+
+<script>
+export default {
+  name: 'Quiz',
+  data() {
+    return {
+      exerciseQuestion: 'What is 1 + 1?',
+      selectedResponse: '1',
+      range: [1, 2, 3, 4],
+      submitted: false,
+    };
+  },
+  methods: {
+    submitAnswer(response) {
+      // send current question, plus whether response was correct
+      console.log(response);
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import HelloWorld from '@/components/HelloWorld';
+import Quiz from '@/components/Quiz';
+
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Quiz',
+      component: Quiz,
+    },
+    {
+      path: '/coachreport',
+      name: 'CoachReport',
+      component: HelloWorld,
+    },
+  ],
+});
