4d32f9b275d97c3ed83b7923d39080b47d2c04a0
Jonathan Boiser <jboiser@gmail.com>
2018-01-23 14:38:23 -0800

    Add questions

---
+++ src/assets/questions.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    question: 'What two sports make up the Biathlon?',
+    options: [
+      'Ice dancing and a snowshoe obstacle course',
+      'Ski jumping and bobsledding',
+      'Cross-country skiing and rifle shooting',
+      'Archery and snowboarding',
+    ],
+    answer: 2,
+  },
+  {
+    question: 'In what country did ski jumping originate?',
+    options: [
+      'Colombia',
+      'Switzerland',
+      'Bali',
+      'Norway',
+    ],
+    answer: 3,
+  },
+  {
+    question: 'Where were the first modern Olympic Winter Games held?',
+    options: [
+      'Stockholm, Sweden',
+      'Moscow, Russia',
+      'Salt Lake City, UT',
+      'Chamonix, France',
+    ],
+    answer: 3,
+  },
+  {
+    question: 'What are the only two countries south of the equator to win medals at the Winter Olympics Games?',
+    options: [
+      'Australia and New Zealand',
+      'South Africa and Argentina',
+      'Australia and Argentina',
+      'New Zealand and South Africa',
+    ],
+    answer: 0,
+  },
+  {
+    question: 'What country has won both the most gold medals, and the most medals overall in the Winter Olympic Games?',
+    options: [
+      'Canada',
+      'Norway',
+      'Russia/USSR',
+      'United States',
+    ],
+    answer: 1,
+  },
+];
